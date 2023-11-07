a69abd35a789169453efb2941516a54d09455236
shoptorshi <shoptorshiria78@gmail.com>
2023-11-07 12:38:40 +0600

    api for assignment submission created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(express.json());
 const corsConfig ={
   origin:"*",
@@ -31,6 +31,7 @@ async function run() {
     // await client.connect();
    const featuresCollection = client.db("KnowledgeJunction").collection("features");
    const allAssignmentCollection = client.db("KnowledgeJunction").collection("AllAssignment");
+   const allSubmittedCollection = client.db("KnowledgeJunction").collection("allSubmission");
 
   //  get features data
    app.get('/api/v1/features', async(req,res)=>{
@@ -57,8 +58,22 @@ async function run() {
     console.log(error)
    }
   })
-  // post Assignment data
+  
+  // get single assignment Data
+  app.get('/api/v1/assignment/:id', async(req, res)=>{
+    try{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allAssignmentCollection.findOne(query);
+      res.send(result); 
+      console.log(result) 
 
+    }catch(error){
+       console.log(error)
+    }
+  })
+  
+  //create assignment data
   app.post('/api/v1/createAssignment', async(req, res)=>{
     try{
       const newAssignment = req.body;
@@ -66,7 +81,45 @@ async function run() {
       res.send(result);
     }
     catch(error){
-    
+    console.log(error)
+    }
+  })
+  //  create all submitted data collections
+  app.post('/api/v1/submittedAssignment', async(req, res)=>{
+    try{
+      const newSubmission = req.body;
+      const result = await allSubmittedCollection.insertOne(newSubmission);
+      res.send(result)
+    }
+    catch(error){
+      console.log(error)
+      }
+  })
+
+  // update assignment data
+  app.put('/api/v1/updateAssignment', async(req, res)=>{
+    try{
+      const assignmentData = req.body;
+      const id = req.params.id;
+      const filter = { _id : new ObjectId(id)};
+      const options = { upsert: true };
+      const updateAssignment = {
+        $set: {
+              title:assignmentData.title,
+              description:assignmentData.description,
+              marks:assignmentData.marks,
+              img:assignmentData.img,
+              dueDate:assignmentData.dueDate,
+              difficulty:assignmentData.difficulty,
+          }
+      }
+
+      const result = await allAssignmentCollection.updateOne(filter,updateAssignment, options)
+      res.send(result);
+
+    }catch(error){
+      console.log(error)
+
     }
   })
 
